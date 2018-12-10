23bac2d39615e9591a1befc256283a3df4b5269b
Prashant <bankarprashant17@gmail.com>
2018-12-10 21:35:59 +0530

    + Splash activity added.

---
+++ app/src/main/java/com/prashantb/ecommercetest/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.prashantb.ecommercetest.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.prashantb.ecommercetest.R;
+import com.prashantb.ecommercetest.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
